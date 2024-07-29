2118c100c014b28698e8a93ba51ab9b41dd52a1c
NeyoOfficial <damian.soborak@onet.pl>
2024-07-29 21:52:00 +0200

    Add files via upload

---
+++ K_i_K_WinForm/Kon_Kli_Zad/Klienci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_i_K_WinForm.KontrahenciZadania
+{
+    public class Klienci
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Imię { get; set; } = string.Empty;
+        public string Nazwisko { get; set; } = string.Empty;
+    }
+}
---
+++ K_i_K_WinForm/Program.cs
@@ -0,0 +1,12 @@
+namespace K_i_K_WinForm
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
