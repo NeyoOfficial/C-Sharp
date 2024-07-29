17ec662f8abbfb7137e7c38b9cf91eba5bb15396
NeyoOfficial <damian.soborak@onet.pl>
2024-07-29 12:29:15 +0200

    Delete Klienci_i_Kontrahenci Wersja Rozszerzona directory

--- Klienci_i_Kontrahenci Wersja Rozszerzona/Klienci.cs
+++
@@ -1,9 +0,0 @@
-﻿using Klienci_i_Kontrahenci_Wersja_Rozszerzona;
-
-public class Klient
-{
-    public int ID { get; set; }
-    public string Imię { get; set; } = string.Empty;
-    public string Nazwisko { get; set; } = string.Empty;
-    public List<Zadanie> Zadania { get; set; } = new List<Zadanie>();
-}
--- Klienci_i_Kontrahenci Wersja Rozszerzona/Zadanie.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Klienci_i_Kontrahenci_Wersja_Rozszerzona
-{
-    public class Zadanie
-    {
-        public string Nazwa { get; set; } = string.Empty;
-        public string Opis { get; set; } = string.Empty;
-    }
-}
